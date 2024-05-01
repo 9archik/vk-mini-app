a8dee5b874fd3a5d32fe7e539c53ae127313c6b7
9archik <Pochtamedvedya00@mail.ru>
2024-05-01 16:04:57 +0300

    Добавил скрытие и открытие загруженных ответов

--- src/enteties/comments/UI/DeleteComment/deleteComment.tsx
+++ src/enteties/comments/UI/DeleteComment/deleteComment.tsx
@@ -1,8 +1,15 @@
 import { FC } from 'react';
+import { Text } from '@vkontakte/vkui';
 // import { DeleteCommentType } from '../../Model/interfaces';
 
 const DeleteComment = () => {
-	return <></>;
+	return (
+		<div style={{ margin: '10px 0 0 0' }}>
+			<Text weight="3" aria-level={3}>
+				Комментарий был удален
+			</Text>
+		</div>
+	);
 };
 
 export default DeleteComment;
--- src/widgets/comments/UI/comment/comment.tsx
+++ src/widgets/comments/UI/comment/comment.tsx
@@ -15,6 +15,8 @@ interface ICommentBlock {
 const CommentBlock: FC<ICommentBlock> = ({ comment, addComment }) => {
 	const { text, id, by, time, kids, childComments, deleted } = comment;
 	const [showChild, setShowChild] = useState(false);
+	const [loadingChild, setLoadingChild] = useState(false);
+	const [isGetChild, setIsGetChild] = useState(false);
 
 	return (
 		<Div className={styles.container}>
@@ -28,31 +30,55 @@ const CommentBlock: FC<ICommentBlock> = ({ comment, addComment }) => {
 				<DeleteComment />
 			)}
 
-			{kids && kids?.length > 0 && childComments.length === 0 && (
-				<Button
-					style={{ margin: '10px 0 0 0' }}
-					onClick={() => {
-						if (typeof addComment === 'function') {
-							getCommentsByIds(kids).then((res) => {
-								for (let i = 0; i < res.length; i++) {
-									let el: ICommentItem = {
-										...res[i],
-										parentNodeId: null,
-										childComments: [],
-										isRootNode: true,
-									};
+			{kids && kids?.length > 0 && childComments.length === 0 ? (
+				<>
+					<Button
+						style={{ margin: '10px 0 0 0' }}
+						loading={loadingChild}
+						onClick={() => {
+							if (typeof addComment === 'function') {
+								setLoadingChild(true);
+								getCommentsByIds(kids)
+									.then((res) => {
+										for (let i = 0; i < res.length; i++) {
+											let el: ICommentItem = {
+												...res[i],
+												parentNodeId: null,
+												childComments: [],
+												isRootNode: true,
+											};
 
-									addComment(id, el);
-								}
-							});
-						}
-					}}
-					type={'link'}>
-					Показать ответы
-				</Button>
+											addComment(id, el);
+										}
+
+										setLoadingChild(false);
+										setIsGetChild(true);
+										setShowChild(true);
+									})
+									.catch(() => {});
+							}
+						}}
+						type={'outline'}>
+						Загрузить ответы
+					</Button>
+				</>
+			) : (
+				<>
+					{childComments.length > 0 && (
+						<Button
+							style={{ margin: '10px 0 0 0' }}
+							type={'outline'}
+							onClick={() => {
+								setShowChild(!showChild);
+							}}>
+							{showChild ? 'Скрыть ответы' : 'Показать ответы'}
+						</Button>
+					)}
+				</>
 			)}
 
 			{childComments.length > 0 &&
+				showChild &&
 				childComments.map((el) => {
 					if (typeof el !== 'number') {
 						return <CommentBlock comment={el} addComment={addComment} />;
